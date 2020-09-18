38b1917010fd7e824eac83ee66f44a461d8dec16
Tomasz <fidostomasz@gmail.com>
2020-09-18 22:05:28 +0200

    FootballApp: adding methods getting competitions and matches in a getting data class.

--- src/footballApp/getDataFromApi.ts
+++ src/footballApp/getDataFromApi.ts
@@ -1,4 +1,5 @@
 import * as moment from 'moment';
+import * as fetch from 'isomorphic-fetch';
 
 export class GetData {
     private startDate: moment.Moment;
@@ -12,19 +13,35 @@ export class GetData {
         return fromDate;
     }
 
-    getAvailableCompetitions(token: string): {}[] {
-        const availableCompetitions = [];
-        return availableCompetitions;
+    async getAvailableCompetitions(token: string): Promise<{}[]> {
+        const availableCompetitionsResponse =
+            await fetch(
+                'https://api.football-data.org/v2/competitions/?plan=TIER_ONE', 
+                {
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'X-Auth-Token': token
+                    }
+                }
+            );
+        return (await availableCompetitionsResponse.json()).competitions;
     }
     
-    getCompetitionNewMatches(competition: string): {}[] {
-        const matches = [];
-        return matches;
-    }
-
-    getCompetitionMatchesResults(competition: string): {}[] {
-        const matches = [];
-        return matches;
+    async getCompetitionNewMatches(competition: string, startDate: moment.Moment, token: string): Promise<{}[]> {
+        const competitionMatches =
+            await fetch(
+                'https://api.football-data.org/v2/competitions/' + competition +
+                '/matches?season=' + startDate.format('YYYY'),
+                {
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'X-Auth-Token': token
+                    }
+                }
+            );
+        return (await competitionMatches.json()).matches;
     }
 
 }
